955d10cb932f69724c474dac46b4a5a1607c857b
Crowdbotics <engineering@crowdbotics.com>
2023-11-08 15:29:04 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "swiftplanner-app-d-127774",
+  url: "https://swiftplanner-app-d-127774.botics.co",
+  api: "https://swiftplanner-app-d-127774.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
